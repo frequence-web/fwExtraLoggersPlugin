9089ee0db99415ee0458ccd0d82cb01abe16c2c1
Simon Genin <simon@uowis.fr>
2013-03-29 14:27:35 +0100

    Ajout gelf logger

---
+++ lib/log/fwGelfLogger.class.php
@@ -0,0 +1,55 @@
+<?php
+
+require_once __DIR__.'/../../../../lib/vendor/gelf-php/GELFMessage.php';
+require_once __DIR__.'/../../../../lib/vendor/gelf-php/GELFMessagePublisher.php';
+
+/**
+ * @author Yohan Giarelli <yohan@frequence-web.fr>
+ */
+class fwGelfLogger extends sfLogger
+{
+  protected static $levels = array(
+    sfLogger::EMERG   => GELFMessage::EMERGENCY,
+    sfLogger::ALERT   => GELFMessage::ALERT,
+    sfLogger::CRIT    => GELFMessage::CRITICAL,
+    sfLogger::ERR     => GELFMessage::ERROR,
+    sfLogger::WARNING => GELFMessage::WARNING,
+    sfLogger::NOTICE  => GELFMessage::NOTICE,
+    sfLogger::INFO    => GELFMessage::INFO,
+    sfLogger::DEBUG   => GELFMessage::DEBUG,
+  );
+
+  /**
+   * @var GELFMessagePublisher
+   */
+  protected $publisher;
+
+  /**
+   * @{inheritDoc}
+   */
+  public function __construct(sfEventDispatcher $dispatcher, $options = array())
+  {
+    parent::__construct($dispatcher, $options);
+
+    $this->publisher = new GELFMessagePublisher(
+      $this->options['host'],
+      isset($this->options['port']) ? $this->options['port'] : GELFMessagePublisher::GRAYLOG2_DEFAULT_PORT
+    );
+  }
+
+  /**
+   * @{inheritDoc}
+   */
+  protected function doLog($message, $priority)
+  {
+    $message = new GELFMessage;
+    $message
+      ->setFullMessage($message)
+      ->setShortMessage($message)
+      ->setLevel($priority)
+      ->setTimestamp(time())
+      ->setHost(gethostname());
+
+    $this->publisher->publish($message);
+  }
+}
